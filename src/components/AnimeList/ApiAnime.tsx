d1ea7b8a1eb5846a5f7e6063853188c095e11918
DeyCasGuerrero <depordey@gmail.com>
2023-12-06 17:50:43 -0500

    update

--- src/components/AnimeList/ApiAnime.tsx
+++ src/components/AnimeList/ApiAnime.tsx
@@ -1,13 +1,12 @@
 export async function fetchAnimeData(animeId: any) {
     try {
-      const apiUrl = process.env.API_URL;
-      const response = await fetch(`${apiUrl}/${animeId}/full`);
-      const data = await response.json();
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      return data;
+        const apiUrl = process.env.API_URL;
+        const response = await fetch(`${apiUrl}/${animeId}/full`);
+        const data = await response.json();
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        return data;
     } catch (error) {
-      console.log(error);
-      return null;
+        console.log(error);
+        return null;
     }
-  }
-  
+}
